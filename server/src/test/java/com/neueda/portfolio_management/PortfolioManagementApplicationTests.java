672d1d9b776650ab20e9297b875ca95ea60e6a6d
Kamil Kuzara <kamilkuzara@gmail.com>
2025-08-21 13:46:10 +0000

    Initial commit. Set up Spring Boot project

---
+++ server/src/test/java/com/neueda/portfolio_management/PortfolioManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.portfolio_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
